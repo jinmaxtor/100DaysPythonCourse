6ac98febabb195106dd0bf01d47e6265a0a32d1a
Yimmy Quispe Yujra <jin_maxtor@outlook.com>
2023-03-22 07:17:46 -0400

    feat: completado del codigo para el proyecto del dia 2, calculadora de propinas

--- Day 2/project_tip_calculator.py
+++ Day 2/project_tip_calculator.py
@@ -1,5 +1,10 @@
 print("Welcomo to tha tip calculator.")
 bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+number_people = int(input("How many people to split the bill? "))
 
+tip = bill * (tip_percentage / 100)
+total_bill = bill + tip
+bill_per_person = total_bill / number_people
+
+print(f"Each person should pay: ${bill_per_person:.2f}")
